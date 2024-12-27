bc395ba5855444ca6372f6474eec3b173ff0e5f5
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-12-27 11:30:22 -0600

    fix

--- src/testing/test.ts
+++ src/testing/test.ts
@@ -1,3 +1,4 @@
+import { Decimal } from "@prisma/client/runtime/library";
 import { deliveryDriverService } from "src/services/deliveryDriver.service";
 import { restaurantsService } from "src/services/restaurant.service";
 
@@ -6,8 +7,8 @@ const init = async () => {
   const restaurant = restaurants[0];
 
   const deliveryCandidates = await deliveryDriverService.findCandidates(
-    { longitude: restaurant.longitude.toNumber(), latitude: restaurant.latitude.toNumber() },
-    { longitude: -99.239338, latitude: 19.502275 }
+    { longitude: restaurant.longitude, latitude: restaurant.latitude },
+    { longitude: new Decimal(-99.239338), latitude: new Decimal(19.502275) }
   )
 
   console.log(deliveryCandidates, 'deliveryCandidates');
