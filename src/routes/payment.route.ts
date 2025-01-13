38a4e3bddb19f068283fa0206803efb21b6cd824
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-01-12 21:02:32 -0600

    route modified

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -4,8 +4,6 @@ import { paymentController } from '../controllers/payment.controller';
 
 const router = Router();
 
-router.post('/payment-intent', paymentController.createPaymentIntent);
-router.post('/pay', paymentController.pay);
 router.post('/fetch-params', paymentController.generateParams);
 
 export default router;
