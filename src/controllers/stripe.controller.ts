b01f032db303916e8a878fcd4b97bccb12d7bf05
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-01-23 23:47:09 -0600

    fixes?

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -113,6 +113,105 @@ export const stripeController = {
       res.status(400).send(`Webhook Error`);
     }
   },
+  handleWebhook2: async (req: Request, res: Response) => {
+    const sig = req.headers['stripe-signature']!;
+    try {
+      const event = await stripe.webhooks.constructEventAsync(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET2!);
+      switch (event.type) {
+        case 'payment_intent.succeeded': {
+          const paymentIntent = event.data.object;
+          const metadata = paymentIntent.metadata;
+
+          const {
+            user_id,
+            restaurant,
+            total_price,
+            delivery_fee,
+            userLatitude,
+            userLongitude,
+            plaform_fee_amount,
+            delivery_ptg_amount,
+            cart,
+
+            restaurantAmount,
+          } = metadata;
+
+          const order = await orderService.createOne({
+            payment_intent_id: paymentIntent.id,
+            status: 'preparing',
+            payment_status: 'completed',
+            user_id: Number(user_id),
+            restaurant_id: Number(restaurant),
+            total_price: Number(total_price),
+            latitude: Number(userLatitude),
+            longitude: Number(userLongitude),
+
+            // Restaurante
+            amount: Number(restaurantAmount),
+
+            // Delivery driver
+            delivery_ptg_amount: Number(delivery_ptg_amount),
+            delivery_fee: Number(delivery_fee),
+
+            // Plataforma
+            plaform_fee_amount: Number(plaform_fee_amount),
+          })
+
+          const cartItems = JSON.parse(cart);
+          const mappedCartItems = Object.keys(cartItems).map((key) => {
+            const item = cartItems[key];
+            return {
+              order_id: order.id!,
+              product_id: Number(key),
+              quantity: Number(item.quantity),
+              unit_price: Number(item.price),
+            }
+          })
+
+          await orderItemService.createMany(mappedCartItems);
+
+          await webSocketService.emitToRoom('new-order', `business_${order.restaurant_id.toString()}`, { type: 'new-order', order });
+
+          break;
+        }
+        case 'payment_intent.canceled': {
+          const paymentIntent = event.data.object;
+          const orderFound = await orderService.findByPaymentIntentId(paymentIntent.id);
+          if (!orderFound) throw new Error('Order not found');
+          await orderService.updateOne(orderFound.id, { status: 'cancelled', payment_status: 'rejected' });
+          break;
+        }
+        case 'account.updated': {
+          const account = event.data.object;
+          const { id, requirements, future_requirements } = account;
+
+          const adminFound = await adminService.getAdminByStripeId(id);
+          if (!adminFound) throw new Error('Admin not found');
+
+          if (requirements?.disabled_reason || future_requirements?.disabled_reason) {
+            await adminService.updateAdmin(adminFound.id!, { stripe_status: 'error' });
+            await webSocketService.emitToRoom('stripe-status', `admin_${adminFound.id!.toString()}`, { type: 'stripe-status', status: 'error' });
+            break;
+          } else {
+            await adminService.updateAdmin(adminFound.id!, { stripe_status: 'success' });
+            await webSocketService.emitToRoom('stripe-status', `admin_${adminFound.id!.toString()}`, { type: 'stripe-status', status: 'success' });
+          }
+          break;
+        }
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+      }
+      res.status(200).send();
+    }
+    catch (err) {
+      console.error(err);
+      if (err instanceof Error) {
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+      res.status(400).send(`Webhook Error`);
+    }
+  },
   getTransfers: async (req: Request, res: Response) => {
     try {
       const businessId = req.headers['x-business-id'];
--- src/routes/stripe.route.ts
+++ src/routes/stripe.route.ts
@@ -5,6 +5,7 @@ import { requireAuth } from '@clerk/express';
 const router = Router();
 
 router.post('/webhook', express.raw({type: 'application/json'}), stripeController.handleWebhook);
+router.post('/webhook2', express.raw({type: 'application/json'}), stripeController.handleWebhook);
 router.post('/transfers', stripeController.getTransfers);
 router.post('/create-session', stripeController.createSession);
 router.get('/account', requireAuth(), stripeController.getAccount);
