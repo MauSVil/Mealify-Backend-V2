7d4ec74cd5a75ba317ee4cb9bc8feb4742124b82
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-01-25 05:28:55 -0600

    job processor

--- src/job-processor.ts
+++ src/job-processor.ts
@@ -14,12 +14,14 @@ import webSocketService from "./services/webSocket.service";
     const currentTime = Math.floor(Date.now() / 1000);
 
     const expiredOrders = await redisService.zrangebyscore('delayedOrders', 0, currentTime);
+
     if (!expiredOrders || expiredOrders.length === 0) {
       console.log("No se encontraron órdenes vencidas.");
       process.exit(0);
     }
 
     for (const orderId of expiredOrders) {
+      console.log(`Procesando orden vencida #${orderId}`);
       await orderService.updateOne(Number(orderId), { status: "restaurant_delayed" });
       await webSocketService.emitToRoom('message', `order_${orderId}`, { type: 'order_status_change', payload: { status: 'restaurant_delayed' } });
       await discordService.sendMessage('general', `La orden #${orderId} ha sido marcada como retrasada por el restaurante.`);
