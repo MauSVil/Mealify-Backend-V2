a20e670a3a7b1c6d01fd407c16168e4e8174ce0a
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-02-23 03:43:47 -0600

    job-processor

--- src/job-processor.ts
+++ src/job-processor.ts
@@ -2,30 +2,56 @@ import { orderService } from "./services/order.service";
 import { redisService } from "./services/redis.service";
 import webSocketService from "./services/webSocket.service";
 
+async function exitProcess(code: number) {
+  try {
+    console.log("Cerrando conexiones...");
+
+    if (redisService.client) {
+      await redisService.client.quit();
+      console.log("Redis desconectado.");
+    }
+  } catch (error) {
+    console.error("Error al cerrar conexiones:", error);
+  } finally {
+    console.log("Proceso finalizado con código:", code);
+    process.exit(code);
+  }
+}
+
 (async () => {
   try {
+    console.log("Iniciando script...");
     await redisService.connect();
+    console.log("Conectado a Redis.");
 
     const currentTime = Math.floor(Date.now() / 1000);
-
-    const expiredOrders = await redisService.zrangebyscore('delayedOrders', 0, currentTime);
+    const expiredOrders = await redisService.zrangebyscore("delayedOrders", 0, currentTime);
 
     if (!expiredOrders || expiredOrders.length === 0) {
       console.log("No se encontraron órdenes vencidas.");
-      process.exit(0);
+      return exitProcess(0);
     }
 
     for (const orderId of expiredOrders) {
-      console.log(`Procesando orden vencida #${orderId}`);
-      await orderService.updateOne(Number(orderId), { status: "restaurant_delayed", delay_date: new Date() });
-      await webSocketService.emitToRoom('message', `order_${orderId}`, { type: 'order_status_change', payload: { status: 'restaurant_delayed' } });
-      await redisService.zrem("delayedOrders", orderId);
+      try {
+        console.log(`Procesando orden vencida #${orderId}`);
+        await orderService.updateOne(Number(orderId), { status: "restaurant_delayed", delay_date: new Date() });
+
+        await webSocketService.emitToRoom("message", `order_${orderId}`, {
+          type: "order_status_change",
+          payload: { status: "restaurant_delayed" },
+        });
+
+        await redisService.zrem("delayedOrders", orderId);
+        console.log(`Orden #${orderId} procesada.`);
+      } catch (error) {
+        console.error(`Error al procesar orden #${orderId}:`, error);
+      }
     }
 
-    await redisService.client?.quit();
+    exitProcess(0);
   } catch (err) {
-    console.error("Error durante el procesamiento de órdenes vencidas:", err);
+    console.error("Error general:", err);
+    exitProcess(1);
   }
-  console.log("Saliendo del proceso...");
-  process.exit(0);
 })();
