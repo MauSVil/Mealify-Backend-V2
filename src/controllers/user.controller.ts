7730c737eddf709c6802d7087dd6dd80e118bbde
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-03-15 12:40:06 -0600

    updateUser

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -35,6 +35,11 @@ export const userController = {
   },
   updateUser: async (req: Request, res: Response) => {
     try {
+      const { id } = req.params;
+      if (!id) throw new Error("Id is required");
+      const idNumber = parseInt(id);
+      const userUpdated = await userService.updateUser(idNumber, req.body);
+      res.status(200).json(userUpdated);
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ message: error.message });
